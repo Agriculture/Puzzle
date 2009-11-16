3d12cc6c15e4520ef168fc4c4ce59860c3845972
konrad <konrad@xapos.(none)>
2009-11-16 21:44:23 +0100

    complex15 solved in 15s, what a wonderful day :)

--- src/MyPuzzleSolver.java
+++ src/MyPuzzleSolver.java
@@ -1,14 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import puzzlelib.*;
 
 /**
@@ -112,8 +107,8 @@ public class MyPuzzleSolver implements IPuzzleSolver
 	private void IDA(){
 		//init
 		PriorityQueue<Node> queue = new PriorityQueue<Node>();
-		Map<Integer, Integer> hash = new HashMap<Integer, Integer>();
-		hash.put(Arrays.deepHashCode(rootState), 0);
+		HashMap<Integer, Boolean> hash = new HashMap<Integer, Boolean>();
+		hash.put(Arrays.deepHashCode(rootState), null);
 
 		//to be sure that we found something in the last iteration
 		Boolean foundSomethingNew = true;
@@ -122,12 +117,9 @@ public class MyPuzzleSolver implements IPuzzleSolver
 		Node root = new Node(null, rootState, null);
 		//set the minimum, if heuristic always return 0 then the limit is 1
 		currentCostLimit = root.getCost()+1;
-		Boolean expand;
 
 		while(!foundSolution && foundSomethingNew){
 			System.err.println("currentCostLimit: "+currentCostLimit);
-			System.err.println("current size of hash: "+hash.size());
-			System.err.println("expanded Nodes: "+expandedNodes+" in (ms) "+(System.currentTimeMillis()-start));
 
 			foundSomethingNew = false;
 			queue.add(root);
@@ -135,7 +127,6 @@ public class MyPuzzleSolver implements IPuzzleSolver
 			while(!queue.isEmpty()){
 //				System.err.println(queue);
 //				System.err.println(hash);
-				expand = true;
 				Node node = queue.poll();
 
 				if(node.isSolution(node.getState())){
@@ -143,45 +134,26 @@ public class MyPuzzleSolver implements IPuzzleSolver
 					foundSolution = true;
 				}
 
-//				System.err.println("look at "+node+" in hash: "+hash.containsKey(node.hashCode())+" "+node.hashCode());
-				if(hash.containsKey(node.hashCode())){
-					Integer hashValue = hash.get(node.hashCode());
-					//found shorter way
-					//test whether we found a shorter way
-					if( (currentCostLimit-node.getCost()) > hashValue){
-						hash.put(node.hashCode(), currentCostLimit-node.getCost());
-						//search
-					} else {
-						//we have already seen this from a shorter or equal way
-						expand = false;
-					}
-				} else {
-						//found new node
-					foundSomethingNew = true;
-					if((currentCostLimit-node.getCost()) < 1){
-						expand = false;
-					} else {
-						hash.put(node.hashCode(), currentCostLimit-node.getCost());
-					}
-				}
-				
-				//look at this in the next round
-				if(expand && (node.getCost() >= currentCostLimit))
-					foundSomethingNew = true;
-
-				if(expand && (node.getCost() < currentCostLimit)){
+				if(node.getCost() < currentCostLimit){
 //					System.err.println(node);
 					expandedNodes++;
+					hash.put(node.hashCode(), null);
 					List<Direction> moves = node.getMoves();
 					for(Direction move : moves){
 						Node newNode = getNextNode(node, move);
-						queue.add(newNode);
+						if(!hash.containsKey(newNode.hashCode())){
+							queue.add(newNode);
+						}
 					}
+				} else {
+					foundSomethingNew = true;
 				}
 			}
-			currentCostLimit++;
+			System.err.println("current size of hash: "+hash.size());
+			System.err.println("expanded Nodes: "+expandedNodes+" in (ms) "+(System.currentTimeMillis()-start));
+			hash.clear();
+			currentCostLimit+=2;
 		}
-		System.err.println("\nlast size of hash: "+hash.size());
 		System.err.println("used memory: (KiB) "+(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())/1024);
 	}
 
@@ -410,12 +382,21 @@ public class MyPuzzleSolver implements IPuzzleSolver
 
 		@Override
 		public boolean equals(Object obj) {
-			return (obj instanceof Node)
-				&&	Arrays.equals(state,((Node) obj).getState());
+			if (obj instanceof Node){
+				Node o = (Node) obj;
+				for(int i=0; i<size*size; i++)
+					if(state[i] != o.getState()[i])
+						return false;
+				return true;
+			}else{
+				return false;
+			}
+				
 		}
 
 		@Override
 		public int hashCode() {
+			//nicht gut...
 			return Arrays.deepHashCode(state);
 		}
 
